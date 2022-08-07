d1fa291c2a59d0ff5d0d642e8f64e8e76b4d1113
Alejandro Velez-Arce <alejandrovelez-arce@Alejandros-MacBook-Pro.local>
2022-08-07 05:19:58 -0600

    mend

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <h3 className="subtitle">Challenge Orthodoxy</h3>
             <h3 className="title">Innovator, Builder</h3>
             <p className="desc">
-             Welcome to my website. A statically-generated decentralized react application hosted on IPFS and registered on the Ethereum Name Service. 
+             Welcome to my website. A statically-generated decentralized react application hosted on IPFS and registered on the <a href="https://alejandrovelez.eth.link/">Ethereum Name Service</a>. 
             </p>
             <img src="img/signature.png" alt="image" />
           </div>
