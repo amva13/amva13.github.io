e3a8dba1f4ca4e42583f115056798a5d0502f676
Alejandro Velez Arce <alex@alexv24.com>
2024-07-21 21:47:08 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
              Welcome to my website. A statically-generated decentralized react application hosted on IPFS, deployed through Fleek, and registered on the <a href="https://alejandrovelez.eth.link/">Ethereum Name Service</a>. 
             </p> */}
             <p className="desc">
-              Welcome to my website. It is a decentralized static React application deployed on the InterPlanetary File System (IPFS). It is also available in the Ethereum Name Service (down for maintenance). You can learn more about me in the sections below (and can navigate using the side panel). Enjoy the rest of the site!            </p>
+              Welcome to my website (socials on the side panel). It is a decentralized static React application deployed on the InterPlanetary File System (IPFS). It is also available in the Ethereum Name Service (down for maintenance). You can learn more about me in the sections below (and can navigate using the side panel). Enjoy the rest of the site!            </p>
             <img src="img/signature.png" alt="image" />
           </div>
           {/* /Main Title */}
